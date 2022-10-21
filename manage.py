92931c380dd57fd7a06266c426b837436b2b1f54
Panshul Jindal <panshul.jindal2019@vitstudent.ac.in>
2022-10-22 03:27:41 +0530

    Initial deploy

--- manage.py
+++ manage.py
@@ -1,22 +1,144 @@
 #!/usr/bin/env python
-"""Django's command-line utility for administrative tasks."""
-import os
+import socket
+import subprocess
 import sys
+from datetime import datetime
+import json
+import os
+import threading
+import builtins
+from multi.scanner_thread import split_processing
+import logging
+from flask import Flask, render_template, request, redirect, url_for
+
+app = Flask(__name__)
 
 
-def main():
-    """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'portscanner.settings')
+@app.route('/')
+def homepage():
+    return render_template('index.html')
+
+
+exc = getattr(builtins, "IOError", "FileNotFoundError")
+
+# Clear the screen
+# subprocess.call('clear', shell=True)
+@app.route('/dns')
+def dnspage():
+    return render_template('dns.html')
+
+@app.route('/dnsscannerinput',methods=["POST"])
+def dnsscannerinput():
+    if request.method == "POST":
+        ipAddress = request.form["host"]
+        print(ipAddress)
+        from dnsrecord import dnsScan
+        dictionary=dnsScan(ipAddress)
+        total=next(iter(dictionary))
+        addrDict = dictionary[total]
+        for ipAdd in addrDict:
+            print(ipAdd)
+        return render_template('dns.html',ipAddress=ipAddress,addrDict=addrDict,total=total)
+
+    else:
+        return EnvironmentError
+@app.route('/ipscanner')
+def scannerpage():
+    return render_template('ipscanner.html')
+
+@app.route('/ipscannerInput',methods=["POST"])
+def ipscannerinput():
+    if request.method == "POST":
+        ipAddress = request.form["host"]
+        print(ipAddress)
+        from ipscanner import ipAddresScan
+        dictionary=ipAddresScan(ipAddress)
+        total=next(iter(dictionary))
+        addrDict = dictionary[total]
+        return render_template('ipscanner.html',ipAddress=ipAddress,addrDict=addrDict,total=total)
+    else:
+        return EnvironmentError
+
+@app.route('/input', methods=["POST"])
+def input():
+    # Ask for input
+    if request.method == "POST":
+        remoteServer = request.form["host"]
+        remoteServerIP = socket.gethostbyname(remoteServer)
+        range_low = int(request.form["range_low"])
+        range_high = int(request.form["range_high"])
+    else:
+        return EnvironmentError
+
+    # Print a nice banner with information on which host we are about to scan
+    print ("-" * 60)
+    print ("Please wait, scanning remote host....", remoteServerIP)
+    print ("-" * 60)
+
+    # Resolves the relative path to absolute path
+    # [BUG]: https://github.com/vinitshahdeo/PortScanner/issues/19
+    def get_absolute_path(relative_path):
+        dir = os.path.dirname(os.path.abspath(__file__))
+        split_path = relative_path.split("/")
+        absolute_path = os.path.join(dir, *split_path)
+        return absolute_path
+
+    # Check what time the scan started
+    t1 = datetime.now()
+
+    # Getting port range values from config.json
     try:
-        from django.core.management import execute_from_command_line
-    except ImportError as exc:
-        raise ImportError(
-            "Couldn't import Django. Are you sure it's installed and "
-            "available on your PYTHONPATH environment variable? Did you "
-            "forget to activate a virtual environment?"
-        ) from exc
-    execute_from_command_line(sys.argv)
+        with open(get_absolute_path('../config.json')) as config_file:
+            config = json.load(config_file)
+            print (get_absolute_path('../config.json'))
+        # defining number of threads running concurrently
+        CONST_NUM_THREADS = int(config['thread']['count'])
+
+    except IOError:
+        print("config.json file not found")
+    except ValueError:
+        print("Kindly check the json file for appropriateness of range")
+
+    ports = list(range(range_low, range_high, 1))
+    # scanning the port only in range of (range_low, range_high)
+
+    portnum = []
+
+    def scan(ports, range_low, range_high):
+        try:
+            for port in range(range_low, range_high):
+                sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+                result = sock.connect_ex((remoteServerIP, port))
+                if result == 0:
+                    print ("Port {}: 	 Open".format(port))
+                    portnum.append("Port "+str(port))
+                sock.close()
+
+        except KeyboardInterrupt:
+            print ("You pressed Ctrl+C")
+            sys.exit()
+
+        except socket.gaierror:
+            print ('Hostname could not be resolved. Exiting')
+            sys.exit()
+
+        except socket.error:
+            print ("Couldn't connect to server")
+            sys.exit()
+
+    # calling function from scanner_thread.py for multithreading
+    split_processing(ports, CONST_NUM_THREADS, scan, range_low, range_high)
+
+    # Checking the time again
+    t2 = datetime.now()
+
+    # Calculates the difference of time, to see how long it took to run the script
+    total = t2 - t1
+
+    # Printing the information to screen
+    print ('Scanning Completed in: ', total)
+    return render_template('index.html', portnum=portnum, host=remoteServerIP, range_low=range_low, range_high=range_high, total=total)
 
 
 if __name__ == '__main__':
-    main()
+    app.run(debug=True)
